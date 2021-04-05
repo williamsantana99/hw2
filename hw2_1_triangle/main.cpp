be018b46ae8ae4d6963ea31ae01869c728afef3d
williamsantana99 <william.santana.allen@gmail.com>
2021-04-05 07:00:24 -0700

    add main.cpp

--- hw2_1_triangle/main.cpp
+++ hw2_1_triangle/main.cpp
@@ -12,26 +12,26 @@ uLCD_4DGL uLCD(D1, D0, D2); // serial tx, serial rx, reset pin;
 
 int main(void)
 { 
-  int s=100;  
-  
+  int s=100; 
+
+  while(1){
   if(up==1 && confirm==1){
     s=s+20;
   }
 
   if(down==1 && confirm==1){
     s=s-20;
-  }
-  
-  float k=(s/1000);  
+  }  
+  float p=s;  
   ////////*********display uLCD**********////////////////
   uLCD.text_width(4); //4X size text
   uLCD.text_height(4);
   uLCD.locate(1,2);
-  uLCD.printf("%2d",s);
+  uLCD.printf("%3d",s);
 
   ////////*********waveform**************///////////////
-  while(1){
-      for (float i=1.0f; i>=0.0f; i=i-0.1){
+  float k=p/1000.0;
+      for (float i=1.0f; i>=0.0f; i=i-k){
           aout = 0.9*i; 
           printf("%f\n", aout.read() * 3.3f);        
           ThisThread::sleep_for(1ms);
