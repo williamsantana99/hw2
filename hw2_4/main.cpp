864355df46396474913ac8a0847fb50a4ac998c8
williamsantana99 <79575288+williamsantana99@users.noreply.github.com>
2021-04-07 02:35:19 +0800

    Update main.cpp

--- hw2_4/main.cpp
+++ hw2_4/main.cpp
@@ -9,33 +9,26 @@ AnalogOut aout(PA_4);
 
 uLCD_4DGL uLCD(D1, D0, D2); // serial tx, serial rx, reset pin;
 
-void print(float data[], int sample);
-
 int main(void)
 { 
   int s=100;
   int i;       
   int sample = 1000/s;
-  float data[sample];  
+  float data[sample]; 
+  int a=0;
 
-  while(1){
-      
-      //if(up==1 && confirm==1){
-          //s=s+20;
-      //}
-
-      //if(down==1 && confirm==1){
-          //s=s-20;
-      //}           
-      
+  while(a<100){      
+            
     for (int i=0; i<sample; i++){     
           aout = 1.0-float(i*1.0/sample*1.0);                   
-          data[i] = 3.3 * aout;                               
+          data[i] = 3.3 * aout;  
+          a++;
           ThisThread::sleep_for(1ms);    
       }
       
-    }   
-
+   }  
+  
+  /////////////////////////////////////////////////////////
   for(int i=0; i<sample; i++){
         printf("%f\r\n", data[i]);
         //ThisThread::sleep_for(1ms);
@@ -44,13 +37,7 @@ int main(void)
 
   //////////////////////////////////////////////////////
   
-  //uLCD.text_width(4); //4X size text
-  //uLCD.text_height(4);
-  //uLCD.locate(1,2);
-  //uLCD.printf("%3d",s);   
-  
-}
-
+ }
 
 //void print(float data[], int sample){  
     //for(int j=0; j<sample; j++){
